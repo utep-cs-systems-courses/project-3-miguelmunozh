c7caf9152479a87081c56f58bff6b4c180246a86
miguelmunozh <miguelmunozh@outlook.com>
2020-07-18 19:38:54 -0600

    merged p2sw-demo and shape-motion-demo

---
+++ project/README.md
@@ -0,0 +1,20 @@
+## Blinky-Buzzy Toy
+For this lab I used the MSP430 microcontroller and my creativity to dessign
+the toy.
+
+This toy can:
+* generate sounds
+* dinamically change the LEDs that are illuminated
+* implement state machine
+
+## Explanation
+Once you use the command __make_load you can use the four top buttons of the
+MSP430.
+_In button one: you can listen a song in the MSP430.
+_In button two: you can see the LEDs turning on and off to count in binary
+from 0 to 3 (eg. 00,01,10,11) the ones represent the light on red,green as in
+my MSP430.
+_In button three: you can listen a different song but this time the LEDs turn
+on and off at the rithm of the music.
+_In button four: you can see the LEDs turning off and off with certain pattern
+like in blink_demo.
---
+++ project/shapemotion.c
@@ -0,0 +1,112 @@
+/** \file shapemotion.c
+ *  \brief This is a simple shape motion demo.
+ *  This demo creates two layers containing shapes.
+ *  One layer contains a rectangle and the other a circle.
+ *  While the CPU is running the green LED is on, and
+ *  when the screen does not need to be redrawn the CPU
+ *  is turned off along with the green LED.
+ */  
+#include <msp430.h>
+#include <libTimer.h>
+#include <lcdutils.h>
+#include <lcddraw.h>
+#include <p2switches.h>
+#include <shape.h>
+#include <abCircle.h>
+
+#define GREEN_LED BIT6
+
+
+u_int bgColor = COLOR_BLUE;     /**< The background color */
+int redrawScreen = 1;           /**< Boolean for whether screen needs to be redrawn */
+
+Region fieldFence;		/**< fence around playing field  */
+char color = COLOR_BLACK;
+int firstLine = 1;
+int secondLine = 0;
+int thirdLine = 0;
+int fourthLine = 0;
+int col = 10;
+int row = 90;
+/** Initializes everything, enables interrupts and green LED, 
+ *  and handles the rendering for the screen
+ */
+void main()
+{
+  P1DIR |= GREEN_LED;		/**< Green led on when CPU on */		
+  P1OUT |= GREEN_LED;
+
+  configureClocks();
+  lcd_init();
+  shapeInit();
+  p2sw_init(15);
+
+  shapeInit();
+  clearScreen(COLOR_BLUE);
+  //layerInit(&layer0);
+  //layerDraw(&layer0);
+
+
+  //layerGetBounds(&fieldLayer, &fieldFence);
+
+
+  enableWDTInterrupts();      /**< enable periodic interrupt */
+  or_sr(0x8);	              /**< GIE (enable interrupts) */
+
+
+  for(;;) { 
+    while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
+      P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
+      or_sr(0x10);	      /**< CPU OFF */
+    }
+    P1OUT |= GREEN_LED;       /**< Green led on when CPU on */
+    redrawScreen = 0;
+
+    //clearScreen(COLOR_BLUE); not here because this loops runs forever
+    fillRectangle(col,10, 50, 50, COLOR_ORANGE);
+   
+    // movLayerDraw(&ml0, &layer0);
+  }
+}
+
+/** Watchdog timer interrupt handler. 15 interrupts/sec */
+void wdt_c_handler()
+{
+    u_int switches = p2sw_read(), i;
+    char str[5];
+    for (i = 0; i < 4; i++){
+      str[i] = (switches & (1<<i)) ? '-' : '0'+i;
+    }
+  static short count = 0;
+  P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
+  count ++;
+  if (count == 15) {
+    if(col <= 50){ //added to update the shape
+      //clearScreen(COLOR_BLUE); // here is the same problem of the long blink
+      col++;
+    }//else if col == 50 start going back
+    //mlAdvance(&ml0, &fieldFence);
+    if(str[0]=='0'){
+     //clear screen and redraw the figure with a diferent position, same problem than above
+      //clearScreen(COLOR_BLUE);
+      redrawScreen = 1; //not update the screen,
+      /*
+     col -=1;
+     fillRectangle(col,10, 50, 50, COLOR_ORANGE);*/
+    }
+    if(str[1]=='1'){
+     
+     clearScreen(COLOR_BLUE);
+     fillRectangle(20,20, 50, 50, bgColor); // another figure
+     
+    }
+    if(str[2]=='2'){
+      //bgColor = COLOR_WHITE;
+     clearScreen(COLOR_RED);
+     fillRectangle(20,20, 50, 50, COLOR_WHITE);
+    }
+    //clear screen and write something to the screen
+    count = 0;
+  } 
+  P1OUT &= ~GREEN_LED;		    /**< Green LED off when cpu off */
+}
